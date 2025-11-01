625b661260577921dfc94f0fa29f539669360edf
trinhnguyen2k <nguyennhatthuytrinh1311@gmail.com>
2025-11-01 21:45:21 +0700

    setup maven project with sping boot

---
+++ backend/src/main/java/com/cinema/recommender/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.cinema.recommender.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Backend is running!";
+    }
+}
